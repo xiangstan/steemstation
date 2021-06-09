96f0bd6e5b871b9c842204a77da1706a190f0491
Xiang Tan <xtan@uccs.edu>
2021-06-09 16:27:33 -0600

    Update url to webhashhistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "@/views/public/Home";
 
 const routes = [
@@ -53,7 +53,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
