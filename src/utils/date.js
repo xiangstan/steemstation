1d0981004e2b50a4c70f356206d0742f6b3fd7cd
Xiang Tan <xtan@uccs.edu>
2021-06-09 09:46:02 -0600

    Update date library

--- src/utils/date.js
+++ src/utils/date.js
@@ -3,3 +3,12 @@ export function cvtTime(time) {
   const temp = new Date(time +".000Z");
   return temp.getFullYear() + "-" + ("0" + (temp.getMonth() + 1)).slice(-2) + "-" + ("0" + temp.getDate()).slice(-2) + " " + ("0" + temp.getHours()).slice(-2) + ":" + ("0" + temp.getMinutes()).slice(-2);
 }
+
+// get this year
+export function thisYear() {
+  return this.Today().getFullYear();
+}
+// current datetime
+export function isToday() {
+  return new Date();
+}
